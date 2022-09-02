775a158ae8a2daea041baebaf94238b0b2de760a
Maksym-Holovachuk <mgolovachuk@gmail.com>
2022-09-02 23:22:27 +0300

    task3 solved

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import Profile from 'components/Profile/Profile';
 import Statistics from 'components/Statistics/Statistics';
+import FriendList from 'components/FriendList/FriendList';
 
 import user from 'components/Profile/user.json';
 import data from 'components/Statistics/data.json';
+import friends from 'components/FriendList/friends.json';
 
 export const App = () => {
   return (
@@ -28,6 +30,7 @@ export const App = () => {
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
 
+      <FriendList friends={friends} />
       {/* React homework template */}
     </div>
   );
